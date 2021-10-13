d3d1f4df056e356b65c74ae5ba6ee2193ecea8e7
Vladimir Kostetskyi <nobergann@gmail.com>
2021-10-13 15:28:10 +0300

    +

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
         use: [miniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"],
       },
       {
-        test: /\.js$/i,
+        test: /\.jsx?$/i,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
@@ -29,6 +29,10 @@ module.exports = {
   },
 
   plugins: [new miniCssExtractPlugin()],
+
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
 
   devtool: "source-map",
   devServer: {
