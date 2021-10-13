75c13ed7a4e45c84c9389bcd5a1f502142fa4de2
Vladimir Kostetskyi <nobergann@gmail.com>
2021-10-13 14:38:59 +0300

    add css, sass, postcss

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import './styles/index.scss';
+
+const elvenShieldRecipe = {
+    leatherStrips: 2,
+    ironIngot: 1,
+    refinedMoonstone: 4,
+}
+
+const elvenGountletsRecipe = {
+  ...elvenShieldRecipe,
+  leather: 1,
+  refinedMoonstone: 4,
+};
+
+console.log(elvenShieldRecipe);
+console.log(elvenGountletsRecipe);
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const miniCssExtractPlugin = require("mini-css-extract-plugin");
+
+let mode = "development";
+let target = "web";
+
+if (process.env.NODE_ENV === "production") {
+  mode = "production";
+  target = "browserslist";
+}
+
+module.exports = {
+  mode: mode,
+  target: target,
+
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/i,
+        use: [miniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"],
+      },
+      {
+        test: /\.js$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+
+  plugins: [new miniCssExtractPlugin()],
+
+  devtool: "source-map",
+  devServer: {
+    static: "./dist",
+    hot: true,
+  },
+};
