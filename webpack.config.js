f9a6a10bfb6c5ecd43a03815eb64c969d68a4846
Vladimir Kostetskyi <nobergann@gmail.com>
2021-10-14 13:58:15 +0300

    add react refresh for dev

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,40 @@
 const path = require('path');
+const reactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 const miniCssExtractPlugin = require("mini-css-extract-plugin");
 const htmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 let mode = "development";
 let target = "web";
+const plugins = [
+  new CleanWebpackPlugin(),
+  new miniCssExtractPlugin(),
+  // {
+  //       filename: "css/styles.[hash].css",
+  //     }
+  new htmlWebpackPlugin({
+    template: "./src/index.html",
+  }),
+
+];
 
 if (process.env.NODE_ENV === "production") {
   mode = "production";
   target = "browserslist";
+} else {
+  plugins.push(new reactRefreshWebpackPlugin());
 }
 
 module.exports = {
   mode: mode,
   target: target,
 
+  entry: "./src/index.js",
+
   output: {
     path: path.resolve(__dirname, "dist"),
     assetModuleFilename: "images/[hash][ext][query]",
-    filename: "js/index.[fullhash].js",
+    // filename: "js/index.[fullhash].js",
   },
 
   module: {
@@ -54,16 +70,7 @@ module.exports = {
     ],
   },
 
-  plugins: [
-    new CleanWebpackPlugin(),
-    new miniCssExtractPlugin(),
-// {
-//       filename: "css/styles.[hash].css",
-//     }
-    new htmlWebpackPlugin({
-      template: "./src/index.html",
-    }),
-  ],
+  plugins: plugins,
 
   resolve: {
     extensions: [".js", ".jsx"],
